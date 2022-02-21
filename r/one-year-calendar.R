c6e702e431ddb7210e0ef15c25b1c7b347d10c4a
Luis <luistt@pm.me>
2022-02-21 22:41:44 +0100

    Script to generate the one year calendar

---
+++ r/one-year-calendar.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+library(tidyverse)
+library(chron)
+
+setwd("~/git/murcia-air/r")
+source("mapR.R")
+
+df <- read.csv(file="../data/san-basilio-mean-daily-2021.csv", header=TRUE, sep=",")
+
+df['DATE'] <- as.Date(df[,'DATE'], format="%d/%m/%Y")
+df$PM10_Q <- ordered(df$PM10_Q, levels=c("Buena","Razonablemente buena","Regular","Desfavorable","Muy desfavorable", "Extremandamente desfavorable"))
+
+calendarHeat(df$DATE,df$PM10,ncolors = 5, color="g2r")
