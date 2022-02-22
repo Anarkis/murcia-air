c400c0749fa8e673c3c25d1ae7963785f9e02e9b
Luis <luistt@pm.me>
2022-02-22 19:31:55 +0100

    Reading file for 2022

--- r/one-year-calendar.R
+++ r/one-year-calendar.R
@@ -5,7 +5,7 @@ library(chron)
 setwd("~/git/murcia-air/r")
 source("mapR.R")
 
-df <- read.csv(file="../data/san-basilio-mean-daily-2021.csv", header=TRUE, sep=",")
+df <- read.csv(file="../data/san-basilio-mean-daily-2022-2023.csv", header=TRUE, sep=",")
 
 df['DATE'] <- as.Date(df[,'DATE'], format="%d/%m/%Y")
 df$PM10_Q <- ordered(df$PM10_Q, levels=c("Buena","Razonablemente buena","Regular","Desfavorable","Muy desfavorable", "Extremandamente desfavorable"))
